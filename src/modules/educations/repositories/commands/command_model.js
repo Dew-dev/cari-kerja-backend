1746e2f7f91301dae07a7d9d1380aeae315ecc90
tubagusdylan <tubagus.dylanr@gmail.com>
2025-10-14 11:24:52 +0700

    fix: query command educations

--- src/modules/educations/repositories/commands/command_model.js
+++ src/modules/educations/repositories/commands/command_model.js
@@ -2,33 +2,33 @@ const joi = require("joi");
 
 // Schema untuk menambahkan Educations
 const addEducationsParamType = joi.object({
-  worker_id: joi.string().uuid().required(),
+  worker_id: joi.string().required(),
   institution_name: joi.string().max(150).required(),
   degree: joi.string().max(100).required(),
   major: joi.string().max(100).optional().allow(null),
-  start_date: joi.date().required(),
-  end_date: joi.date().optional().allow(null),
+  start_date: joi.string().required(),
+  end_date: joi.string().optional().allow(null),
   is_current: joi.boolean().optional().default(false),
   description: joi.string().optional().allow(null),
 });
 
 // Schema untuk mengupdate Educations
 const updateEducationsParamType = joi.object({
-  id: joi.string().uuid().required(), // Educations id
-  worker_id: joi.string().uuid().required(),
+  id: joi.string().required(), // Educations id
+  worker_id: joi.string().required(),
   institution_name: joi.string().max(150).required(),
   degree: joi.string().max(100).required(),
   major: joi.string().max(100).optional().allow(null),
-  start_date: joi.date().required(),
-  end_date: joi.date().optional().allow(null),
+  start_date: joi.string().required(),
+  end_date: joi.string().optional().allow(null),
   is_current: joi.boolean().optional().default(false),
   description: joi.string().optional().allow(null),
 });
 
 // Schema untuk menghapus Educations
 const deleteEducationsParamType = joi.object({
-  worker_id: joi.string().uuid().required(),
-  id: joi.string().uuid().required(),
+  worker_id: joi.string().required(),
+  id: joi.string().required(),
 });
 
 module.exports = {
