8cc49140274a7e841298868bf04f8f272eac1441
Dew-dev <dewdewd22@gmail.com>
2026-04-17 14:34:59 +0700

    add crash logging for debug

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+process.on("uncaughtException", (err) => {
+  console.error("[CRASH] uncaughtException:", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("[CRASH] unhandledRejection:", reason);
+  process.exit(1);
+});
+
 const Server = require("./src/app/server");
 
 const server = new Server();
